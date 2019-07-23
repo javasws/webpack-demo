13922ed32269bf9a0a093af49c109a5158ef7ef1
快速实践 <2431480685@qq.com>
2019-07-23 17:59:51 +0800

    【添加】给css的.hello增加背景图片，在index.js里给元素增加img标签。

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import $ from 'jquery';
 import './style.css';
+import Icon from './icon.png';
 
 $(document).ready(function () {
 
@@ -11,9 +12,15 @@ $(document).ready(function () {
 function component() {
     var element = document.createElement('div');
 
-    // Lodash, now imported by this script
+    // Lodash, 现在由此脚本导入
     element.innerHTML = _.join(['Hello', 'webpack'], '');
     element.classList.add('hello');
+
+    // 将图像添加到我们现有的div
+    var myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
 
     return element;
 }
