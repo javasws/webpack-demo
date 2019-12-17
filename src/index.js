8a54f2a1a307cf87de78c9774c1723872dcabb97
快速实践 <2431480685@qq.com>
2019-12-18 00:57:24 +0800

    回退版本，清理文件

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import _ from 'lodash';
 import $ from 'jquery';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
 
 $(document).ready(function () {
 
@@ -13,17 +10,7 @@ $(document).ready(function () {
 function component() {
     var element = document.createElement('div');
 
-    // Lodash, 现在由此脚本导入
     element.innerHTML = _.join(['Hello', 'webpack'], '');
-    element.classList.add('hello');
-
-    // 将图像添加到我们现有的div
-    var myIcon = new Image();
-    myIcon.src = Icon;
-
-    element.appendChild(myIcon);
-
-    console.log(Data);
 
     return element;
 }
